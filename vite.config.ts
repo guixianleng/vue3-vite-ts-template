4603ce70d862312b1214f82ec396a5739287d965
LenGxin <18820924791@163.com>
2021-12-27 10:52:41 +0800

    feat: init update

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// npm i --save-dev @types/node
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    // 路径别名
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      },
+      {
+        find: /\/#\//,
+        replacement: pathResolve('types') + '/',
+      },
+    ],
+  },
+  // 支持使用tsx语法
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: 'import { h } from "vue";',
+  },
+})
