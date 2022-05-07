ce4d589a60565ff8ef237c6758f95cff65995d07
LenGxin <18820924791@163.com>
2022-05-07 16:52:36 +0800

    perf: 删除冗余代码

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,12 +1,12 @@
 import type { App } from 'vue'
-import Breadcrumb from './breadcrumb/index.vue'
+import CountdownInput from './CountDown/CountdownInput.vue'
 
-const compList = [Breadcrumb]
+const compList = [CountdownInput]
 
 export function registerGlobComp(app: App) {
   compList.forEach((comp) => {
     app.component(comp?.name || comp?.displayName, comp)
   })
 
-  app.component('Breadcrumb', Breadcrumb)
+  app.component('CountdownInput', CountdownInput)
 }
