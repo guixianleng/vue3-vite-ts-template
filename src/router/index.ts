0b0defd08a1a23de13a8cf7b20ec492fe5120afd
LenGxin <18820924791@163.com>
2022-06-02 10:48:49 +0800

    fix: 修复自动构建

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css'
 
@@ -11,7 +11,7 @@ import Login from './routes/modules/login'
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
